0b38ee3978568c83538d2edf54cb2a0e783dca12
Cameron <132536746+cameronjobson@users.noreply.github.com>
2023-06-23 20:28:34 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
-import './App.css';
+// DateCalc.js
+import './App.css';  // Import the CSS
+import React, { useState } from 'react';
+import { addDays, subDays } from 'date-fns';
 
-function App() {
+function DateCalc() {
+  const [FD, setFD] = useState(new Date()); // Default to current date
+  const [DS, setDS] = useState(0);
+  const [patientPickUpDate, setPatientPickUpDate] = useState(new Date());
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Calculate the new date
+    const newDate = subDays(addDays(FD, DS), 2);
+
+    // Set the new date
+    setPatientPickUpDate(newDate);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div className="container">
+      <form className="form" onSubmit={handleSubmit}>
+        <label className="form-label">
+          FD:
+          <input className="input" type="date" onChange={e => setFD(new Date(`${e.target.value}T12:00:00`))} />
+        </label>
+        <label className="form-label">
+          DS:
+          <input className="input" type="number" onChange={e => setDS(Number(e.target.value))} />
+        </label>
+        <button className="button" type="submit">Calculate</button>
+      </form>
+      <div className="patient-pickup-date">
+        Patient pick up date: {patientPickUpDate.toDateString()}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default DateCalc;
