9dfc251ebff6cc55623598bae8b9999a722d80a0
Cameron <132536746+cameronjobson@users.noreply.github.com>
2023-07-03 15:13:28 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
-// DateCalc.js
-import './App.css';  // Import the CSS
 import React, { useState } from 'react';
-import { addDays, subDays } from 'date-fns';
+import Select from 'react-select';
+import './App.css';
 
-function DateCalc() {
-  const [FD, setFD] = useState(new Date()); // Default to current date
-  const [DS, setDS] = useState(0);
-  const [patientPickUpDate, setPatientPickUpDate] = useState(new Date());
+const drugs = [
+  { value: 0.15, label: 'Codeine' },
+  { value: 2.4, label: 'Fentanyl Transdermal' },
+  { value: 1.0, label: 'Hydrocodone' },
+  { value: 5.0, label: 'Hydromorphone' },
+  { value: 4.7, label: 'Methadone' },
+  { value: 1.0, label: 'Morphine' },
+  { value: 1.5, label: 'Oxycodone' },
+  { value: 3.0, label: 'Oxymorphone' },
+  { value: 0.4, label: 'Tapentadol' },
+  { value: 0.2, label: 'Tramadol' },
+];
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+function App() {
+  const [strengthPerUnit, setStrengthPerUnit] = useState(0);
+  const [units, setUnits] = useState(0);
+  const [daysSupply, setDaysSupply] = useState(0);
+  const [mmePerDay, setMmePerDay] = useState(0);
+  const [selectedDrug, setSelectedDrug] = useState(null);
 
-    // Calculate the new date
-    const newDate = subDays(addDays(FD, DS), 2);
+  const calculateMME = () => {
+    if (!selectedDrug) return; // Don't calculate if no drug is selected
 
-    // Set the new date
-    setPatientPickUpDate(newDate);
-  }
+    let mme = (strengthPerUnit * (units / daysSupply) * selectedDrug.value);
+    setMmePerDay(mme);
+  };
 
   return (
-    <div className="container">
-      <form className="form" onSubmit={handleSubmit}>
-        <label className="form-label">
-          FD:
-          <input className="input" type="date" onChange={e => setFD(new Date(`${e.target.value}T12:00:00`))} />
-        </label>
-        <label className="form-label">
-          DS:
-          <input className="input" type="number" onChange={e => setDS(Number(e.target.value))} />
-        </label>
-        <button className="button" type="submit">Calculate</button>
+    <div className="App">
+      <form>
+        <input type="number" onChange={e => setStrengthPerUnit(e.target.value)} placeholder="Strength per Unit" />
+        <input type="number" onChange={e => setUnits(e.target.value)} placeholder="Units" />
+        <input type="number" onChange={e => setDaysSupply(e.target.value)} placeholder="Days Supply" />
+        <Select 
+          options={drugs} 
+          onChange={setSelectedDrug} 
+          placeholder="Select a drug" 
+          isSearchable 
+        />
+        <button type="button" onClick={calculateMME}>Calculate</button>
+        <div className="resultBox">
+            <p>MME/Day: {mmePerDay}</p>
+        </div>
       </form>
-      <div className="patient-pickup-date">
-        Patient pick up date: {patientPickUpDate.toDateString()}
-      </div>
     </div>
   );
 }
 
-export default DateCalc;
+export default App;
